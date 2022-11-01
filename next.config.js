5ade8fc3c86584d2774b3d35114e5e3e022a6038
benediktharter <benedikt.harter@gmail.com>
2022-11-01 18:40:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
-module.exports = {
-  experimental: {
-    runtime: 'experimental-edge', // 'node.js' (default) | experimental-edge
-  },
-}
+//module.exports = {
+ // experimental: {
+ //   runtime: 'experimental-edge', // 'node.js' (default) | experimental-edge
+ // },
+//}
